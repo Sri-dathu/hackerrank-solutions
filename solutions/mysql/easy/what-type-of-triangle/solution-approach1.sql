270ad8a03d4e9e0b7b51992b9624ef10b022bb4e
Piduru Sridattu Reddy <sridattureddypiduru@gmail.com>
2026-07-24 19:50:39 +0530

    ✅ Type of Triangle [mysql] — approach1

---
+++ solutions/mysql/easy/what-type-of-triangle/solution-approach1.sql
@@ -0,0 +1,22 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/what-type-of-triangle/problem?isFullScreen=true
+-- Problem     Type of Triangle
+-- Difficulty  Easy
+-- Subdomain   Advanced Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-07-24, 07:50 p.m.
+-- ──────────────────────────────────────────────────
+
+/*
+Enter your query here.
+*/
+SELECT
+    CASE
+        WHEN A + B <= C OR A + C <= B OR B + C <= A THEN 'Not A Triangle'
+        WHEN A = B AND B = C THEN 'Equilateral'
+        WHEN A = B OR B = C OR A = C THEN 'Isosceles'
+        ELSE 'Scalene'
+    END
+FROM TRIANGLES;
