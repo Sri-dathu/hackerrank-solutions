7fd0bebf24d69cecf2f7e9f0ac8aab885f0d4509
Piduru Sridattu Reddy <sridattureddypiduru@gmail.com>
2026-07-28 20:07:01 +0530

    ✅ Select By ID [sql] — approach1

---
+++ solutions/sql/easy/select-by-id/solution-approach1.sql
@@ -0,0 +1,15 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/select-by-id/problem?isFullScreen=true
+-- Problem     Select By ID
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    sql
+-- Status      Accepted
+-- Submitted   2026-07-28, 08:06 p.m.
+-- ──────────────────────────────────────────────────
+
+
+
+SELECT * from city 
+where id=1661;
